5b455d7e7bfe5d512288b415f2ee6254a6e76d9d
Eamonn de Leastar <edeleastar@wit.ie>
2020-12-04 08:49:38 +0000

    include id in todo

--- todo.js
+++ todo.js
@@ -1,5 +1,12 @@
 let todoItems = [];
 
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == "x" ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
 function renderTodo(todo) {
   const table = document.getElementById("todo-table");
   const row = table.insertRow(-1);
@@ -14,6 +21,7 @@ function addTodo() {
   const todo = {
     text: todoText,
     date: new Date().toLocaleString("en-IE"),
+    id: uuidv4()
   };
   todoItems.push(todo);
   renderTodo(todo);
